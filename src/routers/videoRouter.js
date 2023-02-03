c33a2a8035df6534177c1772bcd26b433d140d28
Jiwon <chjiwony@gmail.com>
2023-02-03 13:29:35 +0900

    #4.8 URL Parameter
    Number Regulation

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -8,10 +8,10 @@ import {
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/delete", deleteVideo);
-
+videoRouter.get("/:id(\\d+)", see); //:id    parameters, URL에 변수를 담는다
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload); //이제 제일 뒤에 와도 숫자와 구별된다.
+// \d 숫자 + 끝까지 \w 글자
 // 각 js 는 독립적이므로 Video 지우고 다른 js 함수와 같아도 된다.
 export default videoRouter;
