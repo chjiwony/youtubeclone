c3d1412ecea4eb0b0e5590c476a9689b29633324
Jiwon <chjiwony@gmail.com>
2023-02-11 20:41:40 +0900

    #6.12 Callback query

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,49 +1,27 @@
-let videos = [
-  {
-    title: "First Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 1,
-    id: 1,
-  },
-  {
-    title: "Second Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 2,
-  },
-  {
-    title: "Third Video",
-    rating: 5,
-    comments: 2,
-    createdAt: "2minutes ago",
-    views: 59,
-    id: 3,
-  },
-];
+import Video from "../models/Video";
 
-export const trending = (req, res) => {
-  return res.render("home", { pageTitle: "Home", videos });
+export const home = (req, res) => {
+  Video.find({}, (error, videos) => {
+    console.log("error", error);
+    console.log("videos", videos);
+  });
+  return res.render("home", { pageTitle: "Home", videos: [] });
 }; //pug 적용, 괄호 안은 view 이름
 export const watch = (req, res) => {
   const { id } = req.params; //const id = req.params.id;
-  const video = videos[id - 1];
-  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+  return res.render("watch", { pageTitle: `Watching` });
 };
 // {  return res.render("watch");};
 export const getEdit = (req, res) => {
   //painting the form
   const { id } = req.params; //const id = req.params.id;
-  const video = videos[id - 1];
-  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+
+  return res.render("edit", { pageTitle: `Editing` });
 };
 export const postEdit = (req, res) => {
   const { id } = req.params;
   const { title } = req.body;
-  videos[id - 1].title = title;
+
   return res.redirect(`/videos/${id}`);
   // saving the changes
 };
@@ -55,14 +33,6 @@ export const getUpload = (req, res) => {
 export const postUpload = (req, res) => {
   // here we will add a video to the videos array.
   const { title } = req.body;
-  const newVideo = {
-    title,
-    rating: 0,
-    comments: 0,
-    createdAt: "just now",
-    views: 0,
-    id: videos.length + 1,
-  };
-  videos.push(newVideo);
+
   return res.redirect("/");
 };
