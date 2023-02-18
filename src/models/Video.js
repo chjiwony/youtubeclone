5f5187a55ab3098b435dd65adca2332682e93e23
Jiwon <chjiwony@gmail.com>
2023-02-18 17:29:47 +0900

    #6.18

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-  title: { type: String, required: true }, //title: {type:Strig}
-  description: { type: String, required: true },
+  title: { type: String, required: true, trime: true, maxLength: 80 }, //title: {type:Strig}
+  description: { type: String, required: true, trime: true, minLength: 20 },
   createdAt: { type: Date, required: true, default: Date.now },
-  hashtags: [{ type: String }],
+  hashtags: [{ type: String, trim: true }],
   meta: {
     views: { type: Number, default: 0, required: true },
     rating: { type: Number, default: 0, required: true },
