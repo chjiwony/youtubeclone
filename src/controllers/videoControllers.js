fd4a17aa81e967ff155a46e3ea5287605aa596bf
Jiwon <chjiwony@gmail.com>
2023-02-18 15:45:02 +0900

    #6.16 Create Video

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -5,7 +5,7 @@ import Video from "../models/Video";
 // });
 export const home = async (req, res) => {
   const videos = await Video.find({}); //find 를 먼저 가져옴, error는 신경안씀.. 이건 try catch로
-  return res.render("home", { pageTitle: "Home", videos });
+  return res.render("home", { pageTitle: "Home", videos }); //뒤에 뭐가 실행되지 않게 하기 위해 return 함. return 없어도 된다.
 };
 
 export const watch = (req, res) => {
@@ -31,9 +31,23 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   // here we will add a video to the videos array.
-  const { title } = req.body;
-
+  const { title, description, hashtags } = req.body;
+  //방법2 오브젝트 따로 안만들고 자동생성, 그러나 에러발생가능 try/error
+  await Video.create({
+    // const video = new Video({
+    //document 생성 videoSchema
+    title: title, //title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  // await video.save();
+  // 방법1 JS 오브젝트를 만들고 저장
   return res.redirect("/");
 };
