e54c55521a155d25bd87b96a0904773b4f297177
Jiwon <chjiwony@gmail.com>
2023-02-21 20:14:27 +0900

    #6.22 Edit

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,29 +1,41 @@
 import Video from "../models/Video";
 
-// Video.find({}, (error, videos) => {
-//   return res.render("home", {pageTitle:"Home", videos});
-// });
 export const home = async (req, res) => {
-  const videos = await Video.find({}); //find 를 먼저 가져옴, error는 신경안씀.. 이건 try catch로
-  return res.render("home", { pageTitle: "Home", videos }); //뒤에 뭐가 실행되지 않게 하기 위해 return 함. return 없어도 된다.
+  const videos = await Video.find({});
+  return res.render("home", { pageTitle: "Home", videos });
 };
 
 export const watch = async (req, res) => {
-  const { id } = req.params; //const id = req.params.id;
+  const { id } = req.params;
   const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
   return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
 };
-// {  return res.render("watch");};
-export const getEdit = (req, res) => {
-  //painting the form
-  const { id } = req.params; //const id = req.params.id;
 
-  return res.render("edit", { pageTitle: `Editing` });
-};
-export const postEdit = (req, res) => {
+export const getEdit = async (req, res) => {
   const { id } = req.params;
-  const { title } = req.body;
-
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+export const postEdit = async (req, res) => {
+  const { id } = req.params;
+  const { title, description, hashtags } = req.body;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.render("404", { pageTitle: "Video not found" });
+  }
+  await Video.findByIdAndUpdate(id, {
+    title,
+    description,
+    hashtags: hashtags
+      .split(",")
+      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+  });
   return res.redirect(`/videos/${id}`);
   // saving the changes
 };
