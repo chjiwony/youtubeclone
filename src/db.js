c741f94de328321888daa7f4d5967edd3a929ab9
Jiwon <chjiwony@gmail.com>
2023-02-24 14:02:59 +0900

    #7.3 Creating account

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/youtubeclone");
+mongoose.connect("mongodb://127.0.0.1:27017/youtubeclone", {});
 
 const db = mongoose.connection;
 
