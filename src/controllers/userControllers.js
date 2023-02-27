59d72f4ee6204ffd6723b59a750ac59164be0ac3
Jiwon <chjiwony@gmail.com>
2023-02-27 19:19:25 +0900

    #8.1

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -133,11 +133,29 @@ export const finishGithubLogin = async (req, res) => {
     return res.redirect("/login");
   }
 };
-
-export const edit = (req, res) => res.send("Edit User");
-export const see = (req, res) => res.send("See User");
-
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
+export const getEdit = (req, res) => {
+  return res.render("edit-profile", { pageTitle: "Edit Profile" });
+};
+export const postEdit = async (req, res) => {
+  const {
+    session: {
+      user: { _id },
+    },
+    body: { name, email, username, location },
+  } = req;
+  //const id = req.session.user.id
+  // const { name, email, username, location } = req.body;
+  await User.findByIdAndUpdate(_id, {
+    name,
+    email,
+    username,
+    location,
+  });
+  return res.render("edit-profile");
+};
+
+export const see = (req, res) => res.send("See User");
