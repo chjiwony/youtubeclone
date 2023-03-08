39eca1d0d5f2e80a6efc49d31bfffe7e2d469e61
Jiwon <chjiwony@gmail.com>
2023-03-08 19:51:38 +0900

    #14.2 ffmpeg mp4 save

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,3 +1,4 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
@@ -5,10 +6,24 @@ let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  await ffmpeg.load();
+
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile)); //FIle System
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" }); //바이너리 데이터를 사용하려면 버프를 사용해야 한다
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+
   const a = document.createElement("a"); //페이크 다운로드 링크 만들기
-  a.href = videoFile;
-  a.download = "MyRecording.webm";
+
+  a.href = mp4Url;
+  a.download = "MyRecording.mp4";
   document.body.appendChild(a);
   a.click();
 };
