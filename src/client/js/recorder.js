4bc37178c6fbb2a47ca39766b23eb65149c87da5
Jiwon <chjiwony@gmail.com>
2023-03-08 18:51:55 +0900

    #13.4 video download webm

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,15 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {};
+const handleDownload = () => {
+  const a = document.createElement("a"); //페이크 다운로드 링크 만들기
+  a.href = videoFile;
+  a.download = "MyRecording.webm";
+  document.body.appendChild(a);
+  a.click();
+};
 const handleStop = () => {
   startBtn.innerText = "Download Recording";
   startBtn.removeEventListener("click", handleStop);
@@ -17,9 +24,9 @@ const handleStart = () => {
   startBtn.innerText = "Stop Recording";
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
-  recorder = new MediaRecorder(stream);
+  recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
   recorder.ondataavailable = (event) => {
-    const videoFile = URL.createObjectURL(event.data); //브라우저 메모리에서만 사용가능한 URL 만들기
+    videoFile = URL.createObjectURL(event.data); //브라우저 메모리에서만 사용가능한 URL 만들기
     video.srcObject = null;
     video.src = videoFile;
     video.loop = true;
