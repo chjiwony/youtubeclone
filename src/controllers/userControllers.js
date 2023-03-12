e223862fa79b5cb7dc5b929819f8211f0af5f76c
Jiwon <chjiwony@gmail.com>
2023-03-12 22:17:36 +0900

    major fix

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -163,11 +163,7 @@ export const postEdit = async (req, res) => {
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file
-        ? isHeroku
-          ? file.location
-          : "/" + file.path
-        : avatarUrl,
+      avatarUrl: file ? file.location : avatarUrl,
       name,
       email,
       username,
